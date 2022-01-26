b8f7b7ad2487df72272f902a7f629ab234406cbf
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-01-26 14:13:35 +0100

    factory + correctionrepository

--- SensoStat.Repository.Contracts/IPersonRepository.cs
+++ SensoStat.Repository.Contracts/IPersonRepository.cs
@@ -10,32 +10,32 @@
         /// <summary>
         /// Ajoute une nouveau utilsateur.
         /// </summary>
-        /// <param name="people">Utilisateur à ajouter.</param>
-        void Add(People people);
+        /// <param name="person">Utilisateur à ajouter.</param>
+        void Add(Person person);
 
         /// <summary>
         /// Supprime un utilisateur.
         /// </summary>
-        /// <param name="people">Utilisateur à supprimer.</param>
-        void Delete(People people);
+        /// <param name="person">Utilisateur à supprimer.</param>
+        void Delete(Person person);
 
         /// <summary>
         /// Retourne l'utilisateur demandée.
         /// </summary>
         /// <param name="id">Id de l'utilisateur à trouver.</param>
         /// <returns>L'utilisateur ayant l'id demandé si celui-ci est trouvé sinon renvoie null.</returns>
-        People Find(int id);
+        Person Find(int id);
 
         /// <summary>
         /// Retourne toutes les utilisateurs.
         /// </summary>
         /// <returns>Une collection contenant tous les utilisateurs.</returns>
-        IEnumerable<People> FindAll();
+        IEnumerable<Person> FindAll();
 
         /// <summary>
         /// Met à jour un utilisateur.
         /// </summary>
-        /// <param name="people">Utilisateur à modifier.</param>
-        void Update(People people);
+        /// <param name="person">Utilisateur à modifier.</param>
+        void Update(Person person);
     }
 }
