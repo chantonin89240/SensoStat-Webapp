2f59dfbee5c0468ca1d94de48445f55257a8e27d
Vincent Simonin <Vincent.Simonin@diiage.org>
2022-01-26 12:45:18 +0100

    maj layout MVC BO et startup.cs

--- SensoStat.WebApplication/Program.cs
+++ SensoStat.WebApplication/Program.cs
@@ -1,6 +1,21 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace SensoStat.WebApplication
+{
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+}
