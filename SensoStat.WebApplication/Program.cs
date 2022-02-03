1e575f6cddef60e4d66420e5aab35e3e50e0dfc1
MELVYN PERRET <melvyn.perret@diiage.org>
2022-02-03 14:58:51 +0100

    Config WebApplication (prise en charge des variables d'environnement, settings pour l'url de l'api).

--- SensoStat.WebApplication/Program.cs
+++ SensoStat.WebApplication/Program.cs
@@ -27,6 +27,11 @@ namespace SensoStat.WebApplication
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(config =>
+                {
+                    config.AddJsonFile("appsettings.json", optional: true);
+                    config.AddEnvironmentVariables();
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
