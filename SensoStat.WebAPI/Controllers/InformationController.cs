e4b63344de69c934fb857a5f8e161744d6ad6a19
MELVYN PERRET <melvyn.perret@diiage.org>
2022-02-22 11:09:53 +0100

    Création d'une url pour récupérer les informations de l'application.

---
+++ SensoStat.WebAPI/Controllers/InformationController.cs
@@ -0,0 +1,24 @@
+﻿namespace SensoStat.WebAPI.Controllers
+{
+    using System.Reflection;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InformationController : ControllerBase
+    {
+        [HttpGet]
+
+        // GET: InformationController
+        public IActionResult Get()
+        {
+            var info = Assembly.GetEntryAssembly().GetReferencedAssemblies().Select(assembly => new
+            {
+                Name = assembly.Name,
+                Version = assembly.Version,
+            }).ToList();
+
+            return this.Ok(info);
+        }
+    }
+}
