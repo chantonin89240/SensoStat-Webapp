deb3401f6e97191fc22f4957915afedf17445b10
MELVYN PERRET <melvyn.perret@diiage.org>
2022-02-23 16:01:38 +0100

    Création de la route pour poster une réponse.

---
+++ SensoStat.Services/ResponseService.cs
@@ -0,0 +1,31 @@
+﻿namespace SensoStat.Services
+{
+    using SensoStat.Entities;
+    using SensoStat.Models.HttpRequest;
+    using SensoStat.Repository.Contracts;
+    using SensoStat.Services.Contracts;
+
+    public class ResponseService : IResponseService
+    {
+        private readonly IResponseRepository _responseRepository;
+
+        public ResponseService(IResponseRepository responseRepository)
+        {
+            this._responseRepository = responseRepository;
+        }
+
+        public void PostResponse(ResponseRequest responseRequest)
+        {
+            Response response = new Response()
+            {
+                IdPanelist = responseRequest.IdPanelist,
+                IdInstruction = responseRequest.IdInstruction,
+                CommentResponse = responseRequest.CommentResponse,
+                IdProduct = responseRequest.IdProduct,
+                DateResponse = DateTime.Now,
+            };
+
+            this._responseRepository.Add(response);
+        }
+    }
+}
