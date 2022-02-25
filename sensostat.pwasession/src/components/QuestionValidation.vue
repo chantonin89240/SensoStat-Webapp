1bbeb6702a156f4b8da4cf94f5a0fa4b091638e6
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2022-02-25 14:05:27 +0100

    pwa enoie des données

--- sensostat.pwasession/src/components/QuestionValidation.vue
+++ sensostat.pwasession/src/components/QuestionValidation.vue
@@ -3,7 +3,7 @@
         <p>Vous avez r&eacute;pondu :</p>
 
         <div class="form-floating">
-            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea">{{ response }}</textarea>
+            <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" v-model="response">{{ response }}</textarea>
             <label for="floatingTextarea">Comments</label>
         </div>
 
@@ -27,7 +27,8 @@ export default {
     name: 'QuestionValidationComponent',
     methods:{
         nextInstruction() {
-            this.$emit('nextInstruction')
+            this.$emit('envoieResponse', this.response)
+            //this.$emit('nextInstruction')
         },
         reformuler() {
             this.$emit('questionResponse')
